e2108d5ef04b11eea881298c468eb7110e479b4d
Carlos Agarie <carlos.agarie@gmail.com>
2011-10-08 02:14:25 -0300

    UI almost completed :)

--- src/ivcalc.js
+++ src/ivcalc.js
@@ -2,20 +2,119 @@
 // ivCalc : A JavaScript Implementation of the IV calculation from the Pokémon Games
 //
 
-/*
-We need to get the stats and EVs from the UI, get the base stats from the database (currently from the UI)
-and reverse-calculate the IVs
+//
+// The object stats has the information from the UI, making it acessible to the calculator.
+// Its variables are:
+// - level
+// - nature
+// - hp
+// - atk
+// - def
+// - spd
+// - spatk
+// - spdef
+// - hpBaseStat
+// - atkBaseStat
+// - defBaseStat
+// - spdBaseStat
+// - spatkBaseStat
+// - spdefBaseStat
+//
 
-HP = ((2 * BaseStat + IV + (EV / 4)) * Level / 100 + Level + 10)
+"use strict";
 
-HP - 10 - Level = (2 * BaseStat + IV + (EV / 4)) * Level / 100
+//
+// A brute force method is being employed to find the IVs. We have a finite and very small set
+// of possible IVs, so it's no big deal in performance - but I hope it won't be used in more complex
+// programs.
+//
+// Methods
+// ivCalc.hpIvCalc(stats) : Generates an array with the possible IVs for the HP stat.
+// ivCalc.statIvCalc(stats, name) : Generates an array with the possible IVs for the stat selected 
+// with the string "name".
+//
+var ivCalc = (function () {
+	
+	//
+	// This function is the "inverse" of the HP calculation, which is a non-linear
+	// function itself. It returns an array with the possible IVs calculated.
+	// hp = (base_stat * 2 + iv + Math.floor(ev/4)) * (level/100)) + 10 + level
+	//
+	// @PARAM: stats.hp, stats.baseStat, stats.evs and stats.level
+	// @RETURN: possibleIvs = array with the possible values for the HP IV
+	//
+	var hpIvCalc = function (stats) {
+		var possibleIvs = [],
+			i = 0;
+		
+		for (i = 0; i < 32; i += 1) {
+			stats.iv = i;
+			
+			if (calcHp(stats) === stats.hp) {
+				possibleIvs.push(i);
+			}
+		}
+		
+		return possibleIvs;
+	};
 
-Stat = (((2 * BaseStat + IV + (EV / 4)) * Level / 100 + 5) * Nature)
-*/
+	//
+	// This function is the "inverse" of the HP calculation, which is a non-linear
+	// function itself. It returns an array with the possible IVs calculated.
+	// stat = (base_stat * 2 + iv + Math.floor(ev/4)) * (level/100) + 5) * nature)
+	// 
+	// @PARAM: type (atk, def, etc), stats.value, stats.baseStatValue,
+	// stats.evs, stats.nature and stats.level
+	// @RETURN: possibleIvs = array with the possible values for the IV
+	//
+	var statsIvCalc = function (stats, name) {
+		var possibleIvs = [],
+			i = 0;
+		
+		for (i = 0; i < 32; i += 1) {
+			stats.iv = i;
+			
+			if (calcStat(stats, name) === stats.value) {
+				possibleIvs.push(i);
+			}
+		}
+		
+		return possibleIvs;
+	};
+	
+	//
+	// This function is a simple implementation of the HP calculation formula.
+	//
+	var calcHp = function (stats) {
+		return (Math.floor(((stats.baseStat*2 + stats.iv)*stats.level)/100) + 10 + stats.level);
+	};
+
+	//
+	// This function calculates the "name" stat using the parameters found in the stats object.
+	// If "name" is not specified, it WON'T CONSIDER THE NATURE!
+	//
+	var calcStat = function (stats, name) {
+		if (typeof name === "string") {
+			return (Math.floor(((stats.baseStat * 2 + stats.iv)*stats.level)/100) + 5) * natures.multiplier(stats.nature, name);
+		}
+		else {
+			return (Math.floor(((stats.baseStat * 2 + stats.iv)*stats.level)/100) + 5);
+		}
+	};
+	
+	return {
+		hpIvCalc : hpIvCalc,
+		statsIvCalc : statsIvCalc
+	};
+})(); // ivCalc
 
 //
 // natures is the object responsible for translating increases and decreases given the
 // nature's name.
+//
+// Methods
+// natures.multiplier(nature, name): Generates the stat multiplier for the given "name" stat;
+// use stat.nature!
 //
 var natures = (function () {
 	var info = {
@@ -135,85 +234,3 @@ var natures = (function () {
 	};
 
 })(); // natures
-
-var ivCalc = (function () {
-	
-	//
-	// This function is the "inverse" of the HP calculation, which is a non-linear
-	// function itself. It returns an array with the possible IVs calculated.
-	// hp = (base_stat * 2 + iv + Math.floor(ev/4)) * (level/100)) + 10 + level
-	// 
-	// A brute force method is being employed to find the IVs. We have a finite and very small set
-	// of possible IVs, so it's no big deal in performance - but I hope it won't be used in more complex
-	// programs.
-	//
-	// @PARAM: stats.hp, stats.baseStat, stats.evs and stats.level
-	// @RETURN: possibleIvs = array with the possible values for the HP IV
-	//
-	var hpIvCalc = function (stats) {
-		var possibleIvs = [],
-			evs = stats.evs || 0,
-			i = 0;
-		
-		for (i = 0; i < 32; i += 1) {
-			stats.iv = i;
-			
-			if (calcHp(stats) === stats.hp) {
-				possibleIvs.push(i);
-			}
-		}
-		
-		return possibleIvs;
-	};
-
-	//
-	// This function is the "inverse" of the HP calculation, which is a non-linear
-	// function itself. It returns an array with the possible IVs calculated.
-	// stat = (base_stat * 2 + iv + Math.floor(ev/4)) * (level/100) + 5) * nature)
-	// 
-	// A brute force method is being employed to find the IVs. We have a finite and very small set
-	// of possible IVs, so it's no big deal in performance - but I hope it won't be used in more complex
-	// programs.
-	//
-	// @PARAM: type (atk, def, etc), stats.value, stats.baseStatValue,
-	// stats.evs, stats.nature and stats.level
-	// @RETURN: possibleIvs = array with the possible values for the IV
-	//
-	var statsIvCalc = function (stats, type) {
-		var possibleIvs = [],
-			evs = stats.evs || 0,
-			name = type;
-			i = 0;
-		
-		for (i = 0; i < 32; i += 1) {
-			stats.iv = i;
-			
-			if (calcStat(stats, name) === stats.value) {
-				possibleIvs.push(i);
-			}
-		}
-		
-		return possibleIvs;
-	};
-	
-	var calcHp = function (stats) {
-		return (Math.floor(((stats.baseStat*2 + stats.iv + Math.floor(stats.evs/4))*stats.level)/100) + 10 + stats.level);
-	};
-
-	//
-	// name: atk, def, spd, spatk, spdef
-	//
-	var calcStat = function (stats, name) {
-		if (typeof name === "string") {
-			return (Math.floor(((stats.baseStat * 2 + stats.iv + Math.floor(stats.evs/4))*stats.level)/100) + 5) * natures.multiplier(stats.nature, name);
-		}
-		else {
-			return (Math.floor(((stats.baseStat * 2 + stats.iv + Math.floor(stats.evs/4))*stats.level)/100) + 5);
-		}
-	};
-	
-	return {
-		hpIvCalc : hpIvCalc,
-		statsIvCalc : statsIvCalc
-	};
-})(); // ivCalc
