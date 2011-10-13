cbeb1b70499e8ce50e58991e6281a9104facd789
Carlos Agarie <carlos.agarie@gmail.com>
2011-10-13 16:35:31 -0300

    Added tests for natures.multiplier and createRange

--- tests/ivcalc-test.js
+++ tests/ivcalc-test.js
@@ -5,3 +5,9 @@ IVCalcTest.prototype.test_create_range = function () {
 	assertEquals("createRange must return a range with the first and last elements", "1 - 3", createRange([1,2,3]) );
 	assertEquals("createRange must return the SAME range independently of the array's length", createRange([1,2,3]), createRange([1,2,2,2,2,2,2,3]));
 };
+
+IVCalcTest.prototype.test_natures_multiplier_return_correct_value = function () {
+	assertNumber("natures.multiplier must return a number", natures.multiplier("adamant", "def"));
+	assertEquals("natures.multiplier must return 1.1 if stat is to be boosted", 1.1, natures.multiplier("bold", "def"));
+	assertEquals("natures.multiplier must return 0.9 if stat is to be diminished", 0.9, natures.multiplier("jolly", "spatk"));
+};
