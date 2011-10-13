4b79a6e15e3ca0be4db40e26ef0548568e07d542
Carlos Agarie <carlos.agarie@gmail.com>
2011-10-13 01:07:25 -0300

    JsTestDriver working

--- tests/ivcalc-test.js
+++ tests/ivcalc-test.js
@@ -1,7 +1,7 @@
 IVCalcTest = TestCase("IVCalcTest");
 
 IVCalcTest.prototype.test_create_range = function () {
-	//assertString("createRange must return a string", createRange([1,3,4,5,6,7]) );
+	assertString("createRange must return a string", createRange([1,3,4,5,6,7]) );
 	assertEquals("createRange must return a range with the first and last elements", "1 - 3", createRange([1,2,3]) );
-	assertEquals("createRange must return the SAME range independently of the array's length", createRange([1,2,3]), createRange([1,2,2,2,2,2,2,2]));
+	assertEquals("createRange must return the SAME range independently of the array's length", createRange([1,2,3]), createRange([1,2,2,2,2,2,2,3]));
 };
